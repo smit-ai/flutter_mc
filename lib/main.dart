e9abdbe95cb9aeb117842000f255b24b92b370fd
57U <ming57u@foxmail.com>
2025-08-27 17:59:22 +0800

    update

--- lib/main.dart
+++ lib/main.dart
@@ -42,7 +42,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter GPU 3D',
+      title: 'FlutterCraft',
       home: Scaffold(body: World()),
     );
   }
